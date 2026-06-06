8116179e2ae7705c3bf89c55d91369109069afff
Sameer warsi <Sameerwarsi@123>
2026-06-06 15:40:09 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const review = require("./review.js");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: String,
+
+    image: {
+        url: {
+            type: String,
+            default: "https://www.tourmyindia.com/states/goa/image/beaches-goa.webp",
+            set: (v) =>
+                v === ""
+                ? "https://www.tourmyindia.com/states/goa/image/beaches-goa.webp"
+                : v,
+        },
+
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+    },
+
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
